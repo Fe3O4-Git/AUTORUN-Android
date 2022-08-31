852f7b4116023f8878a1725055fbbd86c1bdab34
Fe3O4-Git <Fe3O4_Live@Outlook.com>
2022-08-31 23:13:06 +0800

    temp change: start app whose returnHome=true first

--- app/src/main/java/lialh4/inf/autorun/AutorunInfService.java
+++ app/src/main/java/lialh4/inf/autorun/AutorunInfService.java
@@ -20,29 +20,33 @@ public class AutorunInfService extends AccessibilityService {
     }
 
     @Override
-    public void onServiceConnected(){
+    public void onServiceConnected() {
         AppUtils appUtils = new AppUtils(this, false);
         LinkedList<AppUtils.App> apps = appUtils.getApps();
-        for(AppUtils.App app: apps){
-            Intent intent = getPackageManager().getLaunchIntentForPackage(app.packageName);
-            if(intent != null) {
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-                if(app.returnHome){
-                    int time = Math.round(1000 * app.delay);
-                    if(time!=0)
-                        try {
-                            Thread.sleep(time);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    Intent intentHome = new Intent(Intent.ACTION_MAIN);
-                    intentHome.addCategory(Intent.CATEGORY_HOME);
-                    intentHome.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    startActivity(intentHome);
-                }
-            }else
-                Toast.makeText(this, getText(R.string.cant_start)+app.packageName, Toast.LENGTH_SHORT).show();
+        for (int i = 0; i < 2; i++) {
+            for (AppUtils.App app : apps) {
+                if ((i == 0 && !app.returnHome) || (i == 1 && app.returnHome))
+                    continue;
+                Intent intent = getPackageManager().getLaunchIntentForPackage(app.packageName);
+                if (intent != null) {
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                    if (app.returnHome) {
+                        int time = Math.round(1000 * app.delay);
+                        if (time != 0)
+                            try {
+                                Thread.sleep(time);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        Intent intentHome = new Intent(Intent.ACTION_MAIN);
+                        intentHome.addCategory(Intent.CATEGORY_HOME);
+                        intentHome.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intentHome);
+                    }
+                } else
+                    Toast.makeText(this, getText(R.string.cant_start) + app.packageName, Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
